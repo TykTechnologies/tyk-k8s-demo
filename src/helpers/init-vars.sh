2a4dfa0ea8163cf92298801362bcc8ada9f29bde
Zaid Albirawi <zaid@tyk.io>
2022-10-26 17:42:49 -0400

    Fix variable imports

--- src/helpers/init-vars.sh
+++ src/helpers/init-vars.sh
@@ -1,6 +1,9 @@
 # Check for .env file, if found, load variables
 if [[ -f .env ]]; then
-  export $(sed -E '/^[A-Za-z_]+=$/d' .env | xargs);
+  file=$(sed -E '/^[A-Z_]+=$/d' .env | xargs);
+  env=$(env | sed -E '/^[A-Z_]+=$/d' | xargs)
+  export $file;
+  export $env;
 else
   logger $ERROR ".env file not found";
   exit 1;
