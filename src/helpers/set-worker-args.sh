f3ddd94454beb831cb438c4ef3a8097052cb84b5
Zaid Albirawi <zaid@tyk.io>
2022-10-26 17:42:23 -0400

    Update the ip value for worker gateways

--- src/helpers/set-worker-args.sh
+++ src/helpers/set-worker-args.sh
@@ -1,6 +1,6 @@
 port=$(kubectl get service/gateway-svc-$tykReleaseName -n $namespace -o jsonpath="{.spec.ports[0].port}");
 
-ip="gateway-svc-$tykReleaseName.$namespace.svc";
+ip="localhost";
 if [[ $LOADBALANCER == $expose ]]; then
   ip=$(kubectl get service/gateway-svc-$tykReleaseName -n $namespace -o jsonpath="{.status.loadBalancer.ingress[0].ip}");
 elif [[ $INGRESS == $expose ]]; then
