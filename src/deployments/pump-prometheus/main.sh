b1ff88ebc561077f054491ce5438ca0b5dd022fb
Zaid Albirawi <zaid@tyk.io>
2022-12-05 20:21:28 -0600

    Add service mapping to prom setup

--- src/deployments/pump-prometheus/main.sh
+++ src/deployments/pump-prometheus/main.sh
@@ -1,4 +1,5 @@
 PROMETHEUS_PUMP_PORT=9091;
+PROMETHEUS_SERVICE_PORT=9080;
 PROMETHEUS_PUMP_PATH="/metrics";
 
 args=(--set "pump.extraEnvs[$pumpCtr].name=TYK_PMP_PUMPS_PROMETHEUS_TYPE" \
@@ -6,9 +7,12 @@ args=(--set "pump.extraEnvs[$pumpCtr].name=TYK_PMP_PUMPS_PROMETHEUS_TYPE" \
   --set "pump.extraEnvs[$(($pumpCtr + 1))].name=TYK_PMP_PUMPS_PROMETHEUS_META_ADDR" \
   --set "pump.extraEnvs[$(($pumpCtr + 1))].value=0.0.0.0:$PROMETHEUS_PUMP_PORT" \
   --set "pump.extraEnvs[$(($pumpCtr + 2))].name=TYK_PMP_PUMPS_PROMETHEUS_META_PATH" \
-  --set "pump.extraEnvs[$(($pumpCtr + 2))].value=$PROMETHEUS_PUMP_PATH");
-pumpCtr+=6;
+  --set "pump.extraEnvs[$(($pumpCtr + 2))].value=$PROMETHEUS_PUMP_PATH" \
+  --set "pump.extraEnvs[$(($pumpCtr + 3))].name=TYK_PMP_PUMPS_PROMETHEUS_META_CUSTOMMETRICS" \
+  --set "pump.extraEnvs[$(($pumpCtr + 3))].value='[{\"name\":\"tyk_http_requests_total\",\"description\":\"Total of API requests\",\"metric_type\":\"counter\",\"labels\":[\"response_code\",\"api_name\",\"method\",\"api_key\",\"alias\",\"path\"]},{\"name\":\"tyk_http_latency\",\"description\":\"Latency of API requests\",\"metric_type\":\"histogram\",\"labels\":[\"type\",\"response_code\",\"api_name\",\"method\",\"api_key\",\"alias\",\"path\"]}]'");
+pumpCtr=$((pumpCtr + 4));
 
+addService "pump-svc-$tykReleaseName";
 addDeploymentArgs "${args[@]}";
 
 helm upgrade $tykReleaseName $TYK_HELM_CHART_PATH/$chart \
@@ -28,10 +32,16 @@ helm repo update > /dev/null;
 prometheusReleaseName="tyk-prometheus";
 checkHelmReleaseExists $prometheusReleaseName;
 
+addService "tyk-prometheus-server";
+
+command="upgrade";
 if ! $releaseExists; then
-  helm install $prometheusReleaseName prometheus-community/prometheus \
-    --set "serverFiles.prometheus\.yml.scrape_configs[0].job_name=tyk" \
-    --set "serverFiles.prometheus\.yml.scrape_configs[0].metrics_path=$PROMETHEUS_PUMP_PATH" \
-    --set "serverFiles.prometheus\.yml.scrape_configs[0].static_configs[0].targets={pump-svc-$tykReleaseName.$namespace.svc:$PROMETHEUS_PUMP_PORT}" \
-    -n $namespace > /dev/null;
+  command="install";
 fi
+
+helm $command $prometheusReleaseName prometheus-community/prometheus \
+  --set "server.service.servicePort=$PROMETHEUS_SERVICE_PORT" \
+  --set "serverFiles.prometheus\.yml.scrape_configs[0].job_name=tyk" \
+  --set "serverFiles.prometheus\.yml.scrape_configs[0].metrics_path=$PROMETHEUS_PUMP_PATH" \
+  --set "serverFiles.prometheus\.yml.scrape_configs[0].static_configs[0].targets={pump-svc-$tykReleaseName.$namespace.svc:$PROMETHEUS_PUMP_PORT}" \
+  -n $namespace > /dev/null;
