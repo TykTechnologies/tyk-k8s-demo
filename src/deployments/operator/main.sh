68a138830996d07bca6b1da077e2307abec57bca
Zaid Albirawi <zaid@tyk.io>
2022-12-23 09:29:34 -0500

    turn off pormetheus on cert-manager

--- src/deployments/operator/main.sh
+++ src/deployments/operator/main.sh
@@ -16,6 +16,7 @@ helm upgrade "$certManagerReleaseName" jetstack/cert-manager \
   --install \
   --version v1.10.1 \
   --set "installCRDs=true" \
+  --set "prometheus.enabled=true" \
   --namespace "$namespace" \
   --wait --debug;
 
