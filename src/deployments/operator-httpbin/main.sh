35b4b479935960b5e0367fdcad94068ac3cb2550
Zaid Albirawi <zaid@tyk.io>
2023-01-16 15:22:00 -0500

    Updates

--- src/deployments/operator-httpbin/main.sh
+++ src/deployments/operator-httpbin/main.sh
@@ -2,6 +2,7 @@ logger "$INFO" "creating Tyk Operator httpbin example...";
 
 setVerbose;
 kubectl apply -f src/deployments/operator-httpbin/httpbin-svc.yaml -n "$namespace" > /dev/null;
+kubectl wait pods -n "$namespace" -l app=httpbin --for condition=Ready --timeout=60s  > /dev/null;
 
 # httpbin key-less
 sed "s/service_url/httpbin-svc.$namespace.svc:8000/g" src/deployments/operator-httpbin/httpbin-keyless-api.yaml | \
