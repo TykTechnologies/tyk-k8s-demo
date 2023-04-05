e1c03834420b63e9b4864de593da2e097796368f
Zaid Albirawi <zaid@tyk.io>
2023-04-05 12:49:28 -0400

    Update orgid

--- src/helpers/up/set-cp-args.sh
+++ src/helpers/up/set-cp-args.sh
@@ -1,4 +1,5 @@
 authToken=$(kubectl get secrets tyk-operator-conf --namespace "$namespace" -o=jsonpath='{.data.TYK_AUTH}' | base64 -d);
+orgID=$(kubectl get secrets tyk-operator-conf --namespace "$namespace" -o=jsonpath='{.data.TYK_ORG}' | base64 -d);
 port=$(kubectl get "service/mdcb-svc-$tykReleaseName-$chart" --namespace "$namespace" -o jsonpath="{.spec.ports[0].port}");
 
 ip="mdcb-svc-$tykReleaseName-$chart.$namespace.svc";
