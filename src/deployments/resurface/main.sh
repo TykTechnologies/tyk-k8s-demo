dbc1e18a290f82167432d6b1ee8c0ebc4a1de888
Zaid Albirawi <zaid@tyk.io>
2023-04-25 22:28:04 -0400

    update

--- src/deployments/resurface/main.sh
+++ src/deployments/resurface/main.sh
@@ -1,7 +1,6 @@
 logger "$INFO" "installing $resurfaceReleaseName in $namespace namespace...";
 
 addService "coordinator";
-addService "worker";
 
 setVerbose;
 helm upgrade "$resurfaceReleaseName" resurfaceio/resurface --version 3.6.0-0.3.0 \
@@ -11,8 +10,6 @@ helm upgrade "$resurfaceReleaseName" resurfaceio/resurface --version 3.6.0-0.3.0
   --set "custom.service.apiexplorer.type=NodePort" \
   --set "custom.service.apiexplorer.port=$RESURFACE_COORDINATOR_SERVICE_PORT" \
   --set "custom.service.flukeserver.port=$RESURFACE_WORKER_SERVICE_PORT" \
-  --set "sniffer.enabled=true" \
-  --set "sniffer.discovery.enabled=true" \
   --namespace "$namespace" \
   --wait --atomic > /dev/null;
 unsetVerbose;
