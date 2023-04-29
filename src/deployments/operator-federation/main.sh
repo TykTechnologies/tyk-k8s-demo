018507a077f0ac6ee8fd93c7ba810f9bbbba34b4
Zaid Albirawi <zaid@tyk.io>
2023-04-29 02:38:21 -0400

    fix fed

--- src/deployments/operator-federation/main.sh
+++ src/deployments/operator-federation/main.sh
@@ -3,14 +3,14 @@ logger "$INFO" "creating Tyk Operator GraphQL Federation examples...";
 setVerbose;
 # Federation - Users Subgraph
 addService "users-subgraph-svc";
-kubectl apply --namespace "$namespace" -f "$selfSignedCertsDeploymentPath/users-subgraph-app.yaml" > /dev/null;
+kubectl apply --namespace "$namespace" -f "$operatorFederationDeploymentPath/users-subgraph-app.yaml" > /dev/null;
 kubectl wait pods --namespace "$namespace" -l app=users-subgraph --for=condition=Ready --timeout=60s  > /dev/null;
-sed "s/replace_service_url/users-subgraph-svc.$namespace.svc:4201/g" "$selfSignedCertsDeploymentPath/users-subgraph-api-template.yaml" | \
+sed "s/replace_service_url/users-subgraph-svc.$namespace.svc:4201/g" "$operatorFederationDeploymentPath/users-subgraph-api-template.yaml" | \
   kubectl apply --namespace "$namespace" -f - > /dev/null;
 
 # Federation - Posts Subgraph
 addService "posts-subgraph-svc";
-kubectl apply --namespace "$namespace" -f "$selfSignedCertsDeploymentPath/posts-subgraph-app.yaml" > /dev/null;
+kubectl apply --namespace "$namespace" -f "$operatorFederationDeploymentPath/posts-subgraph-app.yaml" > /dev/null;
 kubectl wait pods --namespace "$namespace" -l app=posts-subgraph --for=condition=Ready --timeout=60s  > /dev/null;
 sed "s/replace_service_url/posts-subgraph-svc.$namespace.svc:4202/g" "$selfSignedCertsDeploymentPath/posts-subgraph-api-template.yaml" | \
   kubectl apply --namespace "$namespace" -f - > /dev/null;
@@ -29,5 +29,5 @@ kubectl wait pods --namespace "$namespace" -l app=notifications-subgraph --for=c
 sed "s/replace_service_url/notifications-subgraph-svc.$namespace.svc:4204/g" "$selfSignedCertsDeploymentPath/notifications-subgraph-api-template.yaml" | \
   kubectl apply --namespace "$namespace" -f - > /dev/null;
 
-kubectl apply --namespace "$namespace" -f "$selfSignedCertsDeploymentPath/social-media-federation-api.yaml" > /dev/null;
+kubectl apply --namespace "$namespace" -f "$operatorFederationDeploymentPath/social-media-federation-api.yaml" > /dev/null;
 unsetVerbose;
