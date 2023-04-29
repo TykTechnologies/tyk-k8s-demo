c4c6aabf8cda3423c0e6be26d1927ea606eb52cf
Zaid Albirawi <zaid@tyk.io>
2023-04-29 03:26:42 -0400

    fix fed

--- src/deployments/operator-federation/main.sh
+++ src/deployments/operator-federation/main.sh
@@ -12,21 +12,21 @@ sed "s/replace_service_url/users-subgraph-svc.$namespace.svc:4201/g" "$operatorF
 addService "posts-subgraph-svc";
 kubectl apply --namespace "$namespace" -f "$operatorFederationDeploymentPath/posts-subgraph-app.yaml" > /dev/null;
 kubectl wait pods --namespace "$namespace" -l app=posts-subgraph --for=condition=Ready --timeout=60s  > /dev/null;
-sed "s/replace_service_url/posts-subgraph-svc.$namespace.svc:4202/g" "$selfSignedCertsDeploymentPath/posts-subgraph-api-template.yaml" | \
+sed "s/replace_service_url/posts-subgraph-svc.$namespace.svc:4202/g" "$operatorFederationDeploymentPath/posts-subgraph-api-template.yaml" | \
   kubectl apply --namespace "$namespace" -f - > /dev/null;
 
 # Federation - Comments Subgraph
 addService "comments-subgraph-svc";
-kubectl apply --namespace "$namespace" -f "$selfSignedCertsDeploymentPath/comments-subgraph-app.yaml" > /dev/null;
+kubectl apply --namespace "$namespace" -f "$operatorFederationDeploymentPath/comments-subgraph-app.yaml" > /dev/null;
 kubectl wait pods --namespace "$namespace" -l app=comments-subgraph --for=condition=Ready --timeout=60s  > /dev/null;
-sed "s/replace_service_url/comments-subgraph-svc.$namespace.svc:4203/g" "$selfSignedCertsDeploymentPath/comments-subgraph-api-template.yaml" | \
+sed "s/replace_service_url/comments-subgraph-svc.$namespace.svc:4203/g" "$operatorFederationDeploymentPath/comments-subgraph-api-template.yaml" | \
   kubectl apply --namespace "$namespace" -f - > /dev/null;
 
 # Federation - Notifications Subgraph
 addService "notifications-subgraph-svc";
-kubectl apply --namespace "$namespace" -f "$selfSignedCertsDeploymentPath/notifications-subgraph-app.yaml" > /dev/null;
+kubectl apply --namespace "$namespace" -f "$operatorFederationDeploymentPath/notifications-subgraph-app.yaml" > /dev/null;
 kubectl wait pods --namespace "$namespace" -l app=notifications-subgraph --for=condition=Ready --timeout=60s  > /dev/null;
-sed "s/replace_service_url/notifications-subgraph-svc.$namespace.svc:4204/g" "$selfSignedCertsDeploymentPath/notifications-subgraph-api-template.yaml" | \
+sed "s/replace_service_url/notifications-subgraph-svc.$namespace.svc:4204/g" "$operatorFederationDeploymentPath/notifications-subgraph-api-template.yaml" | \
   kubectl apply --namespace "$namespace" -f - > /dev/null;
 
 kubectl apply --namespace "$namespace" -f "$operatorFederationDeploymentPath/social-media-federation-api.yaml" > /dev/null;
