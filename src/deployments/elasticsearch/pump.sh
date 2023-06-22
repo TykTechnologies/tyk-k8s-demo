01adf30a93f7e906ac65a75c4e50560da55bd5e4
Zaid Albirawi <zaid@tyk.io>
2023-06-22 16:54:02 -0400

    Fix pumps

--- src/deployments/elasticsearch/pump.sh
+++ src/deployments/elasticsearch/pump.sh
@@ -1,4 +1,5 @@
-args=(--set "tyk-pump.pump.extraEnvs[$pumpExtraEnvsCtr].name=TYK_PMP_PUMPS_ELASTICSEARCH_TYPE" \
+args=(--set "global.components.pump=true" \
+  --set "tyk-pump.pump.extraEnvs[$pumpExtraEnvsCtr].name=TYK_PMP_PUMPS_ELASTICSEARCH_TYPE" \
   --set "tyk-pump.pump.extraEnvs[$pumpExtraEnvsCtr].value=elasticsearch" \
   --set "tyk-pump.pump.extraEnvs[$(($pumpExtraEnvsCtr + 1))].name=TYK_PMP_PUMPS_ELASTICSEARCH_META_ELASTICSEARCHURL" \
   --set "tyk-pump.pump.extraEnvs[$(($pumpExtraEnvsCtr + 1))].value=http://$elasticsearchReleaseName.$namespace.svc:$ELASTICSEARCH_SERVICE_PORT" \
