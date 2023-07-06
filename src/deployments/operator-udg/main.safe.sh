2b75303e9364c0aa78cc5a31ab9d0ea1488945d6
Zaid Albirawi <zaid@tyk.io>
2023-07-06 14:26:56 -0400

    Update api-test and add graphql endpoints

--- src/deployments/operator-udg/main.safe.sh
+++ src/deployments/operator-udg/main.safe.sh
@@ -2,6 +2,6 @@ operatorUDGDeploymentPath="src/deployments/operator-udg";
 
 if [ -z "$operatorUDGRegistered" ]; then
   operatorUDGRegistered=true;
-  source "src/deployments/operator/main.safe.sh";
+  source "src/deployments/operator-graphql/main.safe.sh";
   source "$operatorUDGDeploymentPath/main.sh";
 fi
