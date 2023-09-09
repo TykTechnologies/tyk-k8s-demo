7600e9f16881f66960e1ef6fe2d09da4103caf93
Zaid Albirawi <zaid@tyk.io>
2023-09-09 01:11:38 -0400

    Update portal setup

--- src/deployments/portal/main.sh
+++ src/deployments/portal/main.sh
@@ -7,10 +7,11 @@ source src/main/storage/pgsql.sh $portalDBName $portalDBPort;
 addService "enterprise-portal-svc-$tykReleaseName";
 addServiceArgs "enterprisePortal";
 
-args=(--set "enterprisePortal.license=$PORTAL_LICENSE" \
-  --set "enterprisePortal.enabled=true" \
-  --set "enterprisePortal.image.tag=$PORTAL_VERSION" \
-  --set "enterprisePortal.storage.database.connectionString=host\=tyk-$portalDBName-postgres-postgresql.$namespace.svc port\=$portalDBPort user\=postgres password\=$PASSWORD database\=$portalDBName sslmode\=disable" \
+args=(--set "global.components.enterprisePortal=true" \
+  --set "tyk-enterprise-portal.license=$PORTAL_LICENSE" \
+  --set "tyk-enterprise-portal.image.tag=$PORTAL_VERSION" \
+  --set "tyk-enterprise-portal.database.type=postgres" \
+  --set "tyk-enterprise-portal.database.connectionString=host\=tyk-$portalDBName-postgres-postgresql.$namespace.svc port\=$portalDBPort user\=postgres password\=$PASSWORD database\=$portalDBName sslmode\=disable" \
   "${portalSecurityContextArgs[@]}");
 
 addDeploymentArgs "${args[@]}";
