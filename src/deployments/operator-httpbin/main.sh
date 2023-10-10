d2dffcd3daa7ba106b0d0c2d015f0323a415cb4a
Zaid Albirawi <zaid@tyk.io>
2023-10-10 23:37:26 +0200

    Fix operator defs

--- src/deployments/operator-httpbin/main.sh
+++ src/deployments/operator-httpbin/main.sh
@@ -7,6 +7,7 @@ kubectl apply -f "$operatorHTTPDeploymentPath/httpbin-svc.yaml" -n "$namespace" 
 kubectl wait pods -n "$namespace" -l app=httpbin --for condition=Ready --timeout="$TYK_TIMEOUT" > /dev/null;
 
 sed "s/replace_service_url/httpbin-svc.$namespace.svc:8000/g" "$operatorHTTPDeploymentPath/api-template.yaml" | \
-sed "s/replace_namespace/$namespace/g" | \
+  sed "s/replace_namespace/$namespace/g" | \
+  sed "s/replace_protocol/$protocol/g" | \
   kubectl apply -n "$namespace" -f - > /dev/null;
 unsetVerbose;
