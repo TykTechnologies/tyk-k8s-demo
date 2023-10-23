cc3c296ef138819dc2805bc4e574efb1286e98fe
Zaid Albirawi <zaid@tyk.io>
2023-10-23 16:12:04 -0400

    Switch to using k6 chart

--- src/deployments/k6/main.safe.sh
+++ src/deployments/k6/main.safe.sh
@@ -1,6 +1,7 @@
 if [ -z "$k6Registered" ]; then
   k6Registered=true;
 
+  k6ReleaseName="k6";
   k6DeploymentPath="src/deployments/k6";
 
   source "$k6DeploymentPath/main.sh";
--- src/deployments/k6/main.sh
+++ src/deployments/k6/main.sh
@@ -1,15 +1,10 @@
-set +e;
-search=$(kubectl get pods -n k6-operator-system 2> /dev/null | awk '{print $1}' | grep -e "^k6-operator-controller-manager-");
-logger "$DEBUG" "k6/operator-exists: search result: $search";
-set -e;
+logger "$INFO" "installing $k6ReleaseName in $namespace namespace...";
 
-if [[ -z $search ]]; then
-  logger "$INFO" "installing k6-operator in $namespace namespace...";
-
-  git clone https://github.com/grafana/k6-operator.git "$k6DeploymentPath/k6-operator" &> /dev/null;
-  dir=$(pwd);
-  cd "$k6DeploymentPath/k6-operator";
-  make deploy > /dev/null;
-  cd "$dir";
-  rm -rf "$k6DeploymentPath/k6-operator";
-fi
+setVerbose;
+helm upgrade "$k6ReleaseName" grafana/k6-operator --version 1.2.0 \
+  --install \
+  --set "namespace.create=false" \
+  --set "authProxy.enabled=false" \
+  --namespace "$namespace" \
+  "${helmFlags[@]}" > /dev/null;
+unsetVerbose;
