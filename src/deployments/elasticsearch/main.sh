56788fe4b84cc93cb456110fa310baedbba199d5
Zaid Albirawi <zaid@tyk.io>
2023-10-23 20:54:21 -0400

    drop es replicas to 1

--- src/deployments/elasticsearch/main.sh
+++ src/deployments/elasticsearch/main.sh
@@ -7,6 +7,10 @@ helm upgrade "$elasticsearchReleaseName" bitnami/elasticsearch --version 19.13.1
   --install \
   --set "global.elasticsearch.service.ports.restAPI=$ELASTICSEARCH_SERVICE_PORT" \
   --set "sysctlImage.enabled=false" \
+  --set "master.replicaCount=1" \
+  --set "data.replicaCount=1" \
+  --set "coordinating.replicaCount=1" \
+  --set "ingest.replicaCount=1" \
   "${elasticsearchSecurityContextArgs[@]}" \
   "${elasticsearchSSLArgs[@]}" \
   "${elasticsearchLoadbalancerArgs[@]}" \
