f5af33d4ac6aef9b1337429462d878ce5c0206c5
Zaid Albirawi <zaid@tyk.io>
2023-11-01 15:18:42 -0400

    fix username and password

--- src/main/tyk-stack.sh
+++ src/main/tyk-stack.sh
@@ -1,14 +1,16 @@
 source src/main/storage/main.sh;
 
 tykReleaseName="tyk-stack";
-args=(--set "global.license.dashboard=$LICENSE" \
-  --set "tyk-bootstrap.adminUser.email=$TYK_USERNAME" \
-  --set "tyk-bootstrap.adminUser.password=$TYK_PASSWORD" \
+args=(
+  --set "global.license.dashboard=$LICENSE" \
+  --set "global.adminUser.email=$TYK_USERNAME" \
+  --set "global.adminUser.password=$TYK_PASSWORD" \
   --set "tyk-gateway.gateway.image.tag=$GATEWAY_VERSION" \
   --set "tyk-gateway.gateway.service.port=8080" \
   --set "tyk-dashboard.dashboard.image.tag=$DASHBOARD_VERSION" \
   --set "tyk-pump.pump.image.tag=$PUMP_VERSION" \
-  --set "tyk-pump.pump.image.repository=tykio/tyk-pump-docker-pub");
+  --set "tyk-pump.pump.image.repository=tykio/tyk-pump-docker-pub" \
+);
 
 addService "dashboard-svc-$tykReleaseName-tyk-dashboard";
 addService "gateway-svc-$tykReleaseName-tyk-gateway";
