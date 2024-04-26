ff6edaf0881423a30d3d10a864c954edde6c7163
Zaid Albirawi <zaid@tyk.io>
2024-04-26 16:09:54 +0200

    Update charts to 1.3

--- src/main/tyk-gateway.sh
+++ src/main/tyk-gateway.sh
@@ -5,7 +5,7 @@ args=(--set "tyk-gateway.gateway.image.repository=tykio/tyk-gateway" \
   --set "tyk-pump.pump.image.repository=tykio/tyk-pump-docker-pub");
 
 tykReleaseName="tyk-oss";
-tykReleaseVersion="1.2.0";
+tykReleaseVersion="1.3.0";
 addService "gateway-svc-$tykReleaseName-tyk-gateway";
 checkTykRelease;
 
