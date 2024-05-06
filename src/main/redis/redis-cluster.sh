a15e7a902c1fe4bd46a6b3d329d28a94a32dd80b
Zaid Albirawi <zaid@tyk.io>
2024-05-06 16:13:57 -0400

    Update Redis predefined resources

--- src/main/redis/redis-cluster.sh
+++ src/main/redis/redis-cluster.sh
@@ -1,17 +1,21 @@
 logger "$DEBUG" "redis-cluster.sh: setting tyk related redis cluster configuration";
-args=(--set "global.redis.addrs[0]=$redisReleaseName-redis-cluster.$namespace.svc:6379" \
+args=(
+  --set "global.redis.addrs[0]=$redisReleaseName-redis-cluster.$namespace.svc:6379" \
   --set "global.redis.pass=$TYK_PASSWORD" \
-  --set "global.redis.enableCluster=true");
+  --set "global.redis.enableCluster=true" \
+);
 
 if [[ $OPENSHIFT == "$flavor" ]]; then
   logger "$DEBUG" "redis-cluster.sh: setting openshift related redis cluster configuration";
-  securityContextArgs=(--set "podSecurityContext.runAsUser=$OS_UID_RANGE" \
+  securityContextArgs=(
+    --set "podSecurityContext.runAsUser=$OS_UID_RANGE" \
     --set "podSecurityContext.fsGroup=$OS_UID_RANGE" \
     --set "podSecurityContext.seccompProfile.type=RuntimeDefault" \
     --set "containerSecurityContext.runAsUser=$OS_UID_RANGE" \
     --set "containerSecurityContext.allowPrivilegeEscalation=false" \
     --set "containerSecurityContext.capabilities.drop[0]=ALL" \
-    --set "containerSecurityContext.seccompProfile.type=RuntimeDefault");
+    --set "containerSecurityContext.seccompProfile.type=RuntimeDefault" \
+  );
 fi
 
 addDeploymentArgs "${args[@]}";
@@ -20,6 +24,7 @@ helm upgrade "$redisReleaseName" bitnami/redis-cluster --version 10.0.1 \
   --install \
   --namespace "$namespace" \
   --set "password=$TYK_PASSWORD" \
+  --set "redis.resourcesPreset=large" \
   "${securityContextArgs[@]}" \
   "${helmFlags[@]}" > /dev/null;
 unsetVerbose;
