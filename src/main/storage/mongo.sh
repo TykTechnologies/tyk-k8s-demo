71a65529d0992d359f5f5b503f299802766eaf2a
Zaid Albirawi <zaid@tyk.io>
2024-07-24 11:02:58 -0400

    Remove custom mongo packages

--- src/main/storage/mongo.sh
+++ src/main/storage/mongo.sh
@@ -16,21 +16,6 @@ setVerbose;
 helm upgrade $mongoReleaseName bitnami/mongodb --version 15.6.15 \
   --install \
   --namespace "$namespace" \
-  --set "image.repository=zalbiraw/mongodb" \
-  --set "image.tag=4.4.15-debian-10" \
-  \
-  --set "volumePermissions.image.repository=zalbiraw/bitnami-shell" \
-  --set "volumePermissions.image.tag=11.0.0-debian-11" \
-  \
-  --set "metrics.image.repository=zalbiraw/mongodb-exporter" \
-  --set "metrics.image.tag=0.35.0-debian-11" \
-  \
-  --set "tls.image.repository=zalbiraw/nginx" \
-  --set "tls.image.tag=1.23.2-debian-11" \
-  \
-  --set "externalAccess.autoDiscovery.image.repository=zalbiraw/kubectl" \
-  --set "externalAccess.autoDiscovery.image.tag=1.25.3-debian-11" \
-  \
   --set "auth.rootPassword=$TYK_PASSWORD" \
   --set "replicaSet.enabled=true" \
   "${securityContextArgs[@]}" \
