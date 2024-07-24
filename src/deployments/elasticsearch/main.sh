4260b9860f9002cca111f4d6dc8464c5d6d86ae9
Zaid Albirawi <zaid@tyk.io>
2024-07-24 19:59:35 -0400

    test

--- src/deployments/elasticsearch/main.sh
+++ src/deployments/elasticsearch/main.sh
@@ -8,9 +8,25 @@ helm upgrade "$elasticsearchReleaseName" bitnami/elasticsearch --version 21.3.5 
   --set "global.elasticsearch.service.ports.restAPI=$ELASTICSEARCH_SERVICE_PORT" \
   --set "sysctlImage.enabled=false" \
   --set "master.replicaCount=1" \
+  --set "master.resources.limits.cpu=0" \
+  --set "master.resources.limits.memory=0" \
+  --set "master.resources.requests.cpu=0" \
+  --set "master.resources.requests.memory=0" \
   --set "data.replicaCount=1" \
+  --set "data.resources.limits.cpu=0" \
+  --set "data.resources.limits.memory=0" \
+  --set "data.resources.requests.cpu=0" \
+  --set "data.resources.requests.memory=0" \
   --set "coordinating.replicaCount=1" \
+  --set "coordinating.resources.limits.cpu=0" \
+  --set "coordinating.resources.limits.memory=0" \
+  --set "coordinating.resources.requests.cpu=0" \
+  --set "coordinating.resources.requests.memory=0" \
   --set "ingest.replicaCount=1" \
+  --set "ingest.resources.limits.cpu=0" \
+  --set "ingest.resources.limits.memory=0" \
+  --set "ingest.resources.requests.cpu=0" \
+  --set "ingest.resources.requests.memory=0" \
   "${elasticsearchSecurityContextArgs[@]}" \
   "${elasticsearchSSLArgs[@]}" \
   "${elasticsearchLoadbalancerArgs[@]}" \
