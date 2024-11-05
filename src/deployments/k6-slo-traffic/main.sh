f4daf88704738c57e95dcad27395ea36aa7915e4
Zaid Albirawi <zaid@tyk.io>
2024-11-05 11:19:45 -0500

    fix namespace change

--- src/deployments/k6-slo-traffic/main.sh
+++ src/deployments/k6-slo-traffic/main.sh
@@ -1,7 +1,7 @@
 logger "$INFO" "generating k6 traffic...";
 port=$(kubectl get --namespace "$namespace" "service/gateway-svc-$tykReleaseName-tyk-gateway" -o json | jq '.spec.ports[0].port');
 sed "s/replace_gateway_url/$protocol:\/\/gateway-svc-$tykReleaseName-tyk-gateway.$namespace.svc:$port/g" "$k6SLOTrafficDeploymentPath/slo-traffic-configmap-template.yaml" | \
-  sed "s/replace_listen_path/$(kubectl get --namespace "tyk" tykoas httpbin-keyless -o json | jq -r '.status.listenPath' | cut -c 2- | sed 's:/$::')/g" | \
+  sed "s/replace_listen_path/$(kubectl get --namespace "$namespace" tykoas httpbin-keyless -o json | jq -r '.status.listenPath' | cut -c 2- | sed 's:/$::')/g" | \
   kubectl apply --namespace "$namespace" -f - > /dev/null;
 
 test_name="k6-load-test-$(date +%s)";
