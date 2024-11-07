f2d2724f7acc986c79c550527c10ea78872c30b1
Zaid Albirawi <zaid@tyk.io>
2024-11-07 12:49:11 -0500

    Use dev operator release to support streams

--- src/deployments/operator/main.sh
+++ src/deployments/operator/main.sh
@@ -4,6 +4,8 @@ setVerbose;
 helm upgrade "$operatorReleaseName" tyk-helm/tyk-operator --version 1.0.0 \
   --install \
   --namespace "$namespace" \
+  --set image.repository=buraksekili/tyk-operator\
+  --set image.tag=wip-streams \
   "${operatorSecurityContextArgs[@]}" \
   "${operatorSSLArgs[@]}" \
   "${helmFlags[@]}" > /dev/null;
