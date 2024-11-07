ca51b7b048df9b88a05026ee6d9023ca7f89d45f
Zaid Albirawi <zaid@tyk.io>
2024-11-07 13:25:51 -0500

    Update opeartor setup

--- src/deployments/operator/main.sh
+++ src/deployments/operator/main.sh
@@ -1,12 +1,6 @@
 logger "$INFO" "installing tyk-operator in $namespace namespace...";
 
 setVerbose;
-helm upgrade "$operatorReleaseName" tyk-helm/tyk-operator --version 1.0.0 \
-  --install \
-  --namespace "$namespace" \
-  --set image.repository=buraksekili/tyk-operator\
-  --set image.tag=wip-streams \
-  "${operatorSecurityContextArgs[@]}" \
-  "${operatorSSLArgs[@]}" \
-  "${helmFlags[@]}" > /dev/null;
+sed "s/replace_namespace/$namespace/g" "$operatorDeploymentPath/manifest.yaml" | \
+  kubectl apply -n "$namespace" -f - > /dev/null;
 unsetVerbose;
