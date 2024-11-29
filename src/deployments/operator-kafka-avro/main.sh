a422825566cfed7f5358b3048512b528b31a4360
Zaid Albirawi <zaid@tyk.io>
2024-11-29 11:26:33 -0500

    fix avro example

--- src/deployments/operator-kafka-avro/main.sh
+++ src/deployments/operator-kafka-avro/main.sh
@@ -5,7 +5,7 @@ upgradeTyk;
 
 setVerbose;
 sed "s/replace_namespace/$namespace/g" "$operatorKafkaAvroDeploymentPath/kafka-producer-app.yaml" | \
-  sed "s/replace_svc_name/$kafkaReleaseName/g" | \
+  sed "s/replace_svc_name/$kafkaReleaseName.$namespace.svc/g" | \
   sed "s/replace_svc_port/$KAFKA_SERVICE_PORT/g" | \
   sed "s/replace_topic/avro/g" | \
   kubectl apply -n "$namespace" -f - > /dev/null;
