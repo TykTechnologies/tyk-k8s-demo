0156fb7a63b75c7546261869ef6c9481e5979172
Zaid Albirawi <zaid@tyk.io>
2025-03-03 13:52:30 -0500

    fix cr deletion

--- src/helpers/down/crds.sh
+++ src/helpers/down/crds.sh
@@ -1,13 +1,17 @@
 set +e;
-policies=$(kubectl get tykpolicies --namespace "$namespace"  2> /dev/null |  awk '{print $1}' | tail -n +2 | tr '\n' ' ');
-apis=$(kubectl get tykapis --namespace "$namespace"  2> /dev/null |  awk '{print $1}' | tail -n +2 | tr '\n' ' ');
-oasapis=$(kubectl get tykoas --namespace "$namespace"  2> /dev/null |  awk '{print $1}' | tail -n +2 | tr '\n' ' ');
-supergraphs=$(kubectl get supergraphs --namespace "$namespace"  2> /dev/null |  awk '{print $1}' | tail -n +2 | tr '\n' ' ');
-subgraphs=$(kubectl get subgraphs --namespace "$namespace"  2> /dev/null |  awk '{print $1}' | tail -n +2 | tr '\n' ' ');
+tykpolicies=$(kubectl get tykpolicies --namespace "$namespace" 2> /dev/null | awk '{print $1}' | tail -n +2 | tr '\n' ' ');
+tykapis=$(kubectl get tykapis --namespace "$namespace" 2> /dev/null | awk '{print $1}' | tail -n +2 | tr '\n' ' ');
+tykoas=$(kubectl get tykoas --namespace "$namespace" 2> /dev/null | awk '{print $1}' | tail -n +2 | tr '\n' ' ');
+tykstreams=$(kubectl get tykstreams --namespace "$namespace" 2> /dev/null | awk '{print $1}' | tail -n +2 | tr '\n' ' ');
+supergraphs=$(kubectl get supergraphs --namespace "$namespace" 2> /dev/null | awk '{print $1}' | tail -n +2 | tr '\n' ' ');
+subgraphs=$(kubectl get subgraphs --namespace "$namespace" 2> /dev/null | awk '{print $1}' | tail -n +2 | tr '\n' ' ');
 
-kubectl delete --namespace "$namespace" tykpolicies $policies &> /dev/null;
-kubectl delete --namespace "$namespace" tykapis $apis &> /dev/null;
-kubectl delete --namespace "$namespace" tykoas $oasapis &> /dev/null;
+setVerbose;
+kubectl delete --namespace "$namespace" tykpolicies $tykpolicies &> /dev/null;
+kubectl delete --namespace "$namespace" tykapis $tykapis &> /dev/null;
+kubectl delete --namespace "$namespace" tykoas $tykoas &> /dev/null;
+kubectl delete --namespace "$namespace" tykstreams $tykstreams &> /dev/null;
 kubectl delete --namespace "$namespace" supergraphs $supergraphs &> /dev/null;
 kubectl delete --namespace "$namespace" subgraphs $subgraphs &> /dev/null;
+unsetVerbose;
 set -e;
